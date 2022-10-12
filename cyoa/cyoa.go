0fc91f9585f7af0a5a0fcbeb008b1a24d8fd92d4
Noah Iniguez <niniguezg@gmail.com>
2022-10-11 23:21:48 -0500

    Add routing for cyoa

--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -15,7 +16,7 @@ type StoryArc struct {
 	}
 }
 
-type Adventure map[string]StoryArc
+type Story map[string]StoryArc
 
 func check(err error) {
 	if err != nil {
@@ -23,14 +24,31 @@ func check(err error) {
 	}
 }
 
+type AdventureHandler struct {
+	Story Story
+}
+
+func (h AdventureHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if path == "/" {
+		arc := h.Story["intro"]
+		fmt.Fprintln(w, arc)
+	} else {
+		arc := h.Story[path[1:]]
+		fmt.Fprintln(w, arc)
+	}
+}
+
 func main() {
 	jsonBytes, err := os.ReadFile("gopher.json")
 	check(err)
-	var adv Adventure
-	err = json.Unmarshal(jsonBytes, &adv)
+
+	var s Story
+	err = json.Unmarshal(jsonBytes, &s)
 	check(err)
 
-	for k, v := range adv {
-		fmt.Println(k, v)
-	}
+	handler := AdventureHandler{s}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", handler)
+
 }
